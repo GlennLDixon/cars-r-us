0978ecbd58b9d43ede05426fbacc02732df3f462
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-11-29 22:31:17 -0600

    finished creating the database

---
+++ scripts/database.js
@@ -0,0 +1,90 @@
+const database = {
+    paintColor: [
+        {
+            id: 1,
+            color: "Silver",
+            price: 1100
+        },
+        {
+            id: 2,
+            color: "Midnight Blue",
+            price: 799
+        },
+        {
+            id: 3,
+            color: "Firebrick Red",
+            price: 1599
+        },
+        {
+            id: 4,
+            color: "Spring Green",
+            price: 650
+        }
+    ],
+    interior: [
+        {
+            id: 1,
+            color: "Beige Fabric",
+            price: 373
+        },
+        {
+            id: 2,
+            color: "Charcoal Fabric",
+            price: 499
+        },
+        {
+            id: 3,
+            color: "White Leather",
+            price: 1099
+        },
+        {
+            id: 4,
+            color: "Black leather",
+            price: 899
+        }
+    ],
+    Technology: [
+        {
+            id: 1,
+            package: "Basic Package (basic sound system)",
+            price: 999
+        },
+        {
+            id: 2,
+            package: "Navigation Package (includes integrated navigation controls)",
+            price: 1599
+        },
+        {
+            id: 3,
+            package: "Visibility Package (includes side and reat cameras)",
+            price: 1999
+        },
+        {
+            id: 4,
+            package: "Ultra Package (includes navigation and visibility packages)",
+            price: 2599
+        }
+    ],
+    wheels = [
+        {
+            id: 1,
+            type: "17-inch pair Radial",
+            price: 399
+        },
+        {
+            id: 2,
+            type: "17-inch pair Radial Black",
+            price: 599
+        },
+        {
+            id: 3,
+            type: "18-inch pair Spoke Silver",
+            price: 899
+        },
+        {
+            id: 4,
+            type: "18-inch pair Spoke Black",
+            price:1099
+        }
+    ]
+}
